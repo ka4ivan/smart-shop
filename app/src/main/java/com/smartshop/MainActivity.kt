029d4eae4b64d17ab30596c648a9eae3d62f6a06
Develop025 <develop025@gmail.com>
2024-11-02 14:45:08 +0200

    fix build by increase targetSDK test compose

--- app/src/main/java/com/smartshop/MainActivity.kt
+++ app/src/main/java/com/smartshop/MainActivity.kt
@@ -1,14 +1,20 @@
 package com.smartshop
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.smartshop.ui.theme.SmartShopTheme
@@ -32,10 +38,28 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
+    "Greeting".logD()
+    var name by remember { mutableStateOf("") }
+
+//    var name:String = ""
+
     Text(
         text = "Hello $name!",
         modifier = modifier
     )
+
+    OutlinedTextField(
+        value = name,
+        onValueChange = { newName: String ->
+            "newName: $newName".logD()
+            name = newName
+        },
+        label = { Text("Name") }
+    )
+}
+
+fun String.logD() {
+    Log.d("SmartShop", this)
 }
 
 @Preview(showBackground = true)
